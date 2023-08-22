ec3230f85b0af63b043ba12e08aa4daf15084028
lijy <lijylee@126.com>
2023-08-22 14:32:26 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import App from '../App';
 import Home from '../pages/Home';
 import Article from '../pages/Article';
+import Editor from '../pages/Editor';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Profile from '../pages/Profile';
+import Settings from '../pages/Settings'
 const routes = [
   {
     path: '/',
@@ -14,6 +19,25 @@ const routes = [
       {
         path: 'article',
         element: <Article />
+      },
+      {
+        path: 'editor',
+        element: <Editor />
+      },
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'register',
+        element: <Register />
+      },
+      {
+        path: 'profile',
+        element: <Profile />
+      }, {
+        path: 'settings',
+        element: <Settings />
       }
     ]
   }
